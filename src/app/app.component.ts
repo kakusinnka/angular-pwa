13bfb24a9f8a6716ebb47510c26c942ba53c3a88
haozhenhua <1305510482@qq.com>
2021-09-18 18:05:54 +0900

    Available and activated updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent  implements OnInit {
 
   ngOnInit() {
 
+      // SwUpdate.isEnabled() 检查是否启用了 Angular Service Worker。
       if (this.swUpdate.isEnabled) {
 
           this.swUpdate.available.subscribe(() => {
@@ -21,6 +22,18 @@ export class AppComponent  implements OnInit {
 
                   window.location.reload();
               }
+          });
+
+          // 只要有新的应用程序版本可用，就会发出 UpdateAvailableEvent 事件。
+          this.swUpdate.available.subscribe(event => {
+            console.log('current version is', event.current);
+            console.log('available version is', event.available);
+          });
+
+          // 每当应用程序更新到新版本时，都会发出 UpdateActivatedEvent 事件。
+          this.swUpdate.activated.subscribe(event => {
+            console.log('old version was', event.previous);
+            console.log('new version is', event.current);
           });
       }        
   }
